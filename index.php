284791727884ab8155155306cc2195f7edfedf29
dkjnna <faridfadilah70@gmail.com>
2024-08-29 20:26:54 +0700

    Menambahkan navbar, home ,sub home tanpa css

--- index.php
+++ index.php
@@ -1,16 +1,79 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="assets/css/main.css">
 </head>
+
 <body>
     <div class="container">
         <!-- Navbar -->
-
+        <?php require('includes/navbar.php') ?>
         <!-- Home -->
-
+        <div class="home" id="home">
+            <img src="assets/images/bg.jfif" alt="" class="bg-home">
+            <div class="foto-home">
+                <div class="foto-bruno">
+                    <img src="assets/images/bruno.png" alt="" srcset="">
+                </div>
+            </div>
+            <div class="content">
+                <h2 class="subtitle">Comming Soon</h2>
+                <h1 class="title">Bruno On Stage</h1>
+                <h5 class="location">Jakarta International Stadium</h5>
+                <div class="informasi">
+                    <p>Tour For The latest Album – ‘24kmagic’. </p>
+                    <p>Sunday, September 13th-14th, 2024</p>
+                </div>
+            </div>
+        </div>
+        <div class="sub-home">
+            <div class="content1">
+                <div class="deskripsi">
+                    <div class="">
+                        <h1>The Bruno’s Concert</h1>
+                        <p>Get ready for an unforgettable night as the sensational
+                            Bruno Mars takes the stage on his highly anticipated world
+                            tour! Known for his electrifying performances and
+                            chart-topping hits, Bruno Mars promises an evening filled
+                            with energy, emotion, and extraordinary music. Join us for
+                            an exhilarating journey through his greatest hits and
+                            experience the magic of live music like never before.
+                            This is a concert event you won’t want to miss!
+                        </p>
+                    </div>
+                    <a href="">Get Ticket</a>
+                </div>
+                <div class="album">
+                    <div class="judul">
+                        <span>A</span>
+                        <span>L</span>
+                        <span>B</span>
+                        <span>U</span>
+                        <span>M</span>
+                    </div>
+                    <div class="foto-album">
+                        <img src="assets/images/24k.jpg" alt="" class="satu">
+                        <img src="assets/images/monyet.jpg" alt="" class="dua">
+                        <img src="assets/images/sonic.jpg" alt="" class="tiga">
+                        <img src="assets/images/roket.jpg" alt="" class="empat">
+                    </div>
+                </div>
+            </div>
+            <div class="contet2">
+                <div class="foto-bruno">
+                    <img src="assets/images/bruno-gitar.jpg" alt="" sizes="" srcset="">
+                </div>
+                <div class="deskripsi">
+                    <h2>Bruno Mars</h2>
+                    <p>is now back to entertain us with his beautiful songs on tour</p>
+                    <h1>Get Your Own Ticket Now</h1>
+                </div>
+            </div>
+        </div>
         <!-- About -->
 
         <!-- Ticket -->
@@ -22,4 +85,5 @@
         <!-- Footer -->
     </div>
 </body>
+
 </html>
